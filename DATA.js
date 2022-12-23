9bfcdb010b5384b3ef87c559bf5cd90b2845083d
Feignbird <97654461+feignbird@users.noreply.github.com>
2022-12-24 02:42:13 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,98 @@
+export const item = {
+    "id": "d6671e7d-efbb-4abb-b52c-87b5db3410ea",
+    "home_feed": [
+        {
+            "id": "adcdb29d-292a-48ab-a1f1-4d2a4a1acc62",
+            "artist_pic" : "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/30.png",
+            "artist_name": "note",
+            "artist_bio": "Language maintain bank fly age course trial while. Various possible prevent just treatment.",
+            "home_carousel": [
+                {
+                    "id": "b6911809-d865-4805-8abc-d256a4d308ce",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/41.png"
+                },
+                {
+                    "id": "87427175-eb98-4d2e-8a6d-22ac89da0757",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/305.png"
+                },
+                {
+                    "id": "d4e3f19a-2df2-4b28-97a8-10cb28f3d4a2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/306.png"
+                },
+                {
+                    "id": "ad9cec95-9cbb-4c05-8096-524395165960",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/197.png"
+                },
+                {
+                    "id": "4e409ce9-2854-4323-b153-320a1c27af5f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/297.png"
+                }
+            ],
+            "art_caption": "direction",
+            "art_description": "Line his cover customer. Organization quite manage enjoy worker.",
+            "artist_age": 20
+        },
+        {
+            "id": "473c1c34-b49c-49db-b374-e12d6ee62186",
+            "artist_pic" : "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/35.png",
+            "artist_name": "consider",
+            "artist_bio": "Bag believe positive must scientist offer. Also card season central.",
+            "home_carousel": [
+                {
+                    "id": "6e3692c9-7323-4057-a528-5dbfe597da5d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/28.png"
+                },
+                {
+                    "id": "ed933c35-a529-498c-87fb-948032ef9db1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/339.png"
+                },
+                {
+                    "id": "53d3fc8b-3033-43d7-80e6-785e0580670f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/225.png"
+                },
+                {
+                    "id": "b7fecaa4-eaa4-4d9b-a702-b7eb2a6db428",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/29.png"
+                },
+                {
+                    "id": "de501147-a252-4683-93bc-4b59d0834606",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/38.png"
+                }
+            ],
+            "art_caption": "focus",
+            "art_description": "Actually after career lose author system across. News already what part image society left always. Live region factor available every move.",
+            "artist_age": 24
+        },
+        {
+            "id": "848f48b5-b425-4f80-acd8-6ae0b6605241",
+            "artist_pic" : "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/50.png",
+            "artist_name": "seat",
+            "artist_bio": "Age sell cover major happen size. His five million piece story pass card. Theory want job exactly term future leg check.",
+            "home_carousel": [
+                {
+                    "id": "2efdd52c-28c9-45ee-9c01-93a21f89c8cd",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/166.png"
+                },
+                {
+                    "id": "cbd46cc1-7bb1-4264-b955-1ce090a72001",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/89.png"
+                },
+                {
+                    "id": "87382725-650a-463d-9784-772a3c4108a0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/53.png"
+                },
+                {
+                    "id": "7f1f5129-197f-4fd4-bff6-4172c45da967",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/260.png"
+                },
+                {
+                    "id": "a30c55f9-415d-4ae7-9ce0-6aaeb2121495",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/156.png"
+                }
+            ],
+            "art_caption": "crime",
+            "art_description": "Establish gun issue decision future fast. Yeah base magazine evidence scientist ask perform.",
+            "artist_age": 27
+        }
+    ]
+}
